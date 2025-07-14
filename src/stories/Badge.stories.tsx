c63f6887dd553171c47cf0f5f255ed172949a6f0
LindaTancini <lindatancini95@gmail.com>
2025-07-14 13:08:36 +0200

    stories: create badge stories

---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import Badge from "../components/Badge/Badge";
+
+const meta: Meta<typeof Badge> = {
+  title: "Badge",
+  component: Badge,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  render: () => <Badge />,
+};
