ed5f1e1967934d04b25a0efe7bf7326725609e25
LindaTancini <lindatancini95@gmail.com>
2025-07-14 13:12:43 +0200

    stories: fix and modify

--- src/stories/Goal.stories.tsx
+++ src/stories/Goal.stories.tsx
@@ -1,17 +1,26 @@
-import Badge from "./assets/badges.png";
-import TabsImg from "./assets/01.gif";
-import React from "react";
+import { Meta, StoryObj } from "@storybook/react-vite";
 
-export default {
+import Badge from "./assets/badges.png";
+import Tabs from "./assets/01.gif";
+
+const meta: Meta = {
   title: "Goal",
 };
 
-export const Badges = () => (
-  <img src={Badge} style={{ width: "25rem" }} alt="Badge" />
-);
+export default meta;
 
-export const Tabs = () => (
+const Template = () => (
   <>
-    <img src={TabsImg} alt="Tabs" />
+    <h1>Goal</h1>
+
+    <h2>Badges</h2>
+    <img src={Badge} style={{ width: "25rem" }} alt="Badge" />
+
+    <h2>Tabs</h2>
+    <img src={Tabs} alt="Tabs" />
   </>
 );
+
+export const Default: StoryObj = {
+  render: () => <Template />,
+};
