5f41ad98631212136e3650acc768fb5815db12c8
LindaTancini <lindatancini95@gmail.com>
2025-07-15 10:38:29 +0200

    feat: import badge.css and add classname

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,8 @@
+import type React from "react";
+import "./Badge.css";
+
 const Badge: React.FC = () => {
-  return <div>Badge</div>;
+  return <div className="badge">Badge</div>;
 };
 
 export default Badge;
