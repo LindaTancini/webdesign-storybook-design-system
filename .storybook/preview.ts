887d766fd1594cd5e342c9885902ae849fbf34a3
LindaTancini <lindatancini95@gmail.com>
2025-08-01 15:12:05 +0200

    script: install addon-theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,12 @@
-import type { Preview } from "@storybook/react-vite";
-//style imports
+/** @type { import('@storybook/react').Preview } */
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+
 import "../src/styles/reset.css";
 import "../src/styles/variables.css";
 import "../src/styles/typography.css";
-const preview: Preview = {
+
+const preview = {
   parameters: {
     controls: {
       matchers: {
@@ -11,14 +14,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: "todo",
-    },
   },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light-theme",
+        dark: "dark-theme",
+      },
+      defaultTheme: "light",
+      parentSelector: "body",
+    }),
+  ],
 };
 
 export default preview;
